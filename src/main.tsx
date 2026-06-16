cd4a83d81e51cc9c77427da5b9c08d0254bc255e
Developer <developer@example.com>
2026-06-16 14:07:24 +0000

    Add EmailJS keys and CDN script

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DailyCheck from './DailyCheck'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DailyCheck />
+  </React.StrictMode>,
+)
