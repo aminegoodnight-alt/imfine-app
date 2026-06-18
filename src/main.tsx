3a109885bb00afb3da1bda88aeb7e6a1ae07d223
aminegoodnight-alt <aminegoodnight@gmail.com>
2026-06-18 16:10:23 +0100

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DailyCheck from './DailyCheck'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <DailyCheck />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />);
